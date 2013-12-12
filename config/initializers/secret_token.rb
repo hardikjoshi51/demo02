e78d59eb33d802a18d08b84786d313a69b0c4887
Hardik Joshi <hrdk.joshi@gmail.com>
2013-12-12 12:49:57 -0500

    Basic Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo02::Application.config.secret_token = '25e2e347c554b8298f0fdd24acb8ecf4c47167d7c198d6fd69e9719a76e1553f0f69d8382bf51530957fd889e308484536e74f9b4c17a6da8dbc076c801efa5d'
